6b14af9fc79cbd9e8d499f0f467e9f569bd426a8
aiotlab-teaching <ktlai@arbor.ee.ntu.edu.tw>
2023-04-14 10:52:35 +0800

    Remove Chinese

--- HouseBuilderTest.java
+++ HouseBuilderTest.java
@@ -1,7 +1,7 @@
 class HouseBuilderTest {
 
     public static void main(String[] args) {
-        House house = new House.HouseBuilder("先鋒大樓").age(1).rooms(100).address("忠孝東路3段1號").build();
+        House house = new House.HouseBuilder("Pioneer Buidling").age(1).rooms(100).address("No. 1, ZhongXiao East Road").build();
         house.printInfo();
     }
 }
